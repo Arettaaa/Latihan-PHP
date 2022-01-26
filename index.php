4610ea7719562e87781b96c82c9ab0f1d8134a3a
Arettaaa <90229562+Arettaaa@users.noreply.github.com>
2022-01-26 09:32:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <html>
-	  <head>
+	<head>
 	    <title>PHP Test</title>
 	  </head>
 	  <body>   
 	 <!-- penanganan form dengan method GET -->
 	<form method="get" action="tampil.php">
-		<label>Masukkan Nama</label><br/>
+	    <label>Nama Siswa</label><br/>
 		<input type="text" name="nama"><br/>
-		<label>Masukkan Usia</label><br/>
-		<input type="text" name="usia"><br/>	
-		<input type="submit" value="oke">
-	</form>
+		<label>Asal Sekolah</label><br/>
+		<input type="text" name="sklh"><br/>
+        <label>Jenis Kelamin</label><br/>
+        <select name="jk" >
+        <option value="Perempuan"> Pilih </option>
+		<option value="Perempuan"> L </option>
+        <option value="Laki-laki"> P </option>
+        </select>
+        <br />
+        <label>No. Telpon</label> 
+		<br/>
+		<input type="text" name="no"><br/>
+		<br/>	
+	    <input type="submit" value="Daftar">
+	    </form>
 	
 	  </body>
 	</html>
