03ba8dfc23e88a891dadb05abab24f1be9ab4c4d
rtaretta <aretta557@gmail.com>
2022-01-26 10:50:08 +0700

    baru

--- coba.php
+++ coba.php
@@ -1,3 +1,12 @@
-<?php
-   echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar PHP - HTML</title>
+</head>
+<body>
+	<h1>Belajar PHP</h1> 
+	<?php
+		echo "Hello World!";
+	?>
+</body>
+</html>
