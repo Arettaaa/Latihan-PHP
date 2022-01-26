198ade7ac28e2bd0663c9b54500a96295e7a24b6
Arettaaa <90229562+Arettaaa@users.noreply.github.com>
2022-01-26 09:30:55 +0700

    Update tampil.php

--- tampil.php
+++ tampil.php
@@ -1,12 +1,38 @@
 <?php 
-//menangkap data nama dengan method post
+
+// menangkap data nama dengan method get
 $nama = $_GET['nama'];
-// menangkap data usia dengan method post
-$usia = $_GET['usia'];
- 
-// menampilkan data nama
-echo "Nama anda adalah " . $nama;
+// menangkap data sekolah dengan method get
+$sklh = $_GET['sklh'];
+// menangkap data kelamin dengan method get
+$jk = $_GET['jk'];
+// menangkap data no. telpon dengan method get
+$no = $_GET['no'];
+
+echo "Selamat <b> $nama </b>, Pendaftaran Berhasil!";
+
 echo "<br/>";
-// menampilkan data usia
-echo "Usia anda adalah " . $usia;
+
+echo "Biodata Anda";
+
+echo "<hr/>";
+
+
+// menampilkan data nama
+echo "Nama: " . $nama;
+echo "<br/>";
+
+// menampilkan data JK
+echo " Jenis Kelamin: " . $jk;
+echo "<br/>";
+
+// menampilkan data SEKOLAH
+echo " Asal SMP: " . $sklh;
+echo "<br/>";
+
+// menampilkan data SEKOLAH
+echo "No. Telp: " . $no;
+echo "<br/>";
+
+
 ?>
